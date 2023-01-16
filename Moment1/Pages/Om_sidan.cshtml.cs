b6d4a679a71af82e34dbb15df3d0d717732e3d4d
Joacim Bäcklund <joba1905@student.miun.se>
2023-01-16 15:04:51 +0100

    Uppdaterat struktur

---
+++ Moment1/Pages/Om_sidan.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moment1.Pages
+{
+    public class Om_sidanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
