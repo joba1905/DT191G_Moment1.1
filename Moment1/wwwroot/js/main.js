16abf7755221056981a316a588f5d62aa7f690ae
Joacim Bäcklund <joba1905@student.miun.se>
2023-01-18 15:52:59 +0100

    Färdigställt uppgift och javascript

---
+++ Moment1/wwwroot/js/main.js
@@ -0,0 +1,27 @@
+﻿//Dagens datum
+var date = new Date();
+
+//Variabel för år
+var year = date.getFullYear();
+
+//Variabel för månad (+1 för januari)
+var mon = date.getMonth() + 1;
+
+//Om bara en siffra lägg på 0 före.
+if (mon < 10) {
+    mon = "0" + mon;
+}
+
+//Variabel för dag
+var day = date.getDate();
+
+//Om bara en siffra lägg på 0 före.
+if (day < 10) {
+    day = "0" + day;
+}
+
+date = year + "-" + mon + "-" + day;
+
+//Skriv ut dagens datum
+document.getElementById("datetext").innerHTML = date;
+
